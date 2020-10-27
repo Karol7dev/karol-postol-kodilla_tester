3827e9617136177cbe7fc58dfcd139706bcc2ddc
Karol Postoł <canterkosmolay@gmail.com>
2020-10-27 11:32:52 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Karol");
+        System.out.println(23);
+        System.out.println('P');
+    }
+}
+
