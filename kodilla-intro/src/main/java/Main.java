d72e876e475c17afc2785d5cb9d890201c9d6d0b
Karol Postoł <canterkosmolay@gmail.com>
2020-10-27 12:56:39 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,15 @@
+class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String intro = "Hello World";
+        System.out.println(myName);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        System.out.println(intro);
+    }
+}
