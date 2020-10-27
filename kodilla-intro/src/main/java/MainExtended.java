13f8130b8931cef45e8b534efd52cba0fc20fd07
Karol Postoł <canterkosmolay@gmail.com>
2020-10-27 18:12:28 +0100

    Initial version

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,15 +1,15 @@
 class Main {
     public static void main(String[] args) {
         String city = "Wrocław";
-        String year = "2077";
-        String releaseDate = "11.20";
+        double year = 2.77;
+        char sigh = 'C';
         String studio = "Cd Projekt Red";
         boolean isGoodProgrammer = true;
         String intro = "We have a city to burn";
         System.out.println(city);
         System.out.println(year);
+        System.out.println(sigh);
         System.out.println(studio);
-        System.out.println(releaseDate);
         System.out.println(isGoodProgrammer);
         System.out.println(intro);
     }
