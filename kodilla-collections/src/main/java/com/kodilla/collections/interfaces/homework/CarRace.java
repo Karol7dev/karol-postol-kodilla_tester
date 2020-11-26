ccc68ca7a448f7a00c939b5119ee118a8ee4663a
Karol Postoł <canterkosmolay@gmail.com>
2020-11-26 12:11:08 +0100

    Initial Version

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -1,6 +1,6 @@
 package com.kodilla.collections.interfaces.homework;
 
-public class CarInterface {
+public class CarRace {
     public static void main(String[] args) {
         Audi audi = new Audi(200);
         doRace(audi);
@@ -13,6 +13,6 @@ public class CarInterface {
     }
 
     private static void doRace(Car car) {
-        System.out.println(car.increaseSpeed() + car.increaseSpeed() + car.increaseSpeed() + car.decreaseSpeed() + car.decreaseSpeed() + car.getSpeed());
+        System.out.println(car.increaseSpeed(car.increaseSpeed(car.increaseSpeed())));
     }
 }
