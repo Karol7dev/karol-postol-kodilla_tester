71a37a5c4e2f691b3b668429211bc787105f6e70
Karol Postoł <canterkosmolay@gmail.com>
2020-12-14 17:14:00 +0100

    Initial Version

---
+++ kodilla-stream/src/main/java/com/kodilla/Optional/OptionalExample.java
@@ -0,0 +1,21 @@
+package com.kodilla.Optional;
+
+import com.kodilla.stream.User;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+//        User user = null;
+       User user = new User("user1", 30, 100, "Test");
+
+        Optional<User> optionalUser = Optional.ofNullable(user);
+
+        optionalUser.ifPresent(u -> System.out.println(u.getUsername()));
+
+//        String username =
+//                optionalUser.orElse(new User("", 0, 0,"")).getUsername();
+//
+//        System.out.println(username);
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/Optional/homework/Application.java
@@ -0,0 +1,31 @@
+package com.kodilla.Optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher("Jon Snow");
+        Teacher teacher2 = new Teacher("John Doe");
+        Teacher teacher3 = new Teacher("Margaret Thatcher");
+
+        Student student1 = new Student("Alex Hath", teacher3);
+        Student student2 = new Student("Sylvanas Windrunner", null);
+        Student student3 = new Student("Laura Greene", teacher1);
+        Student student4 = new Student("Michael Jackson", null);
+
+        List<Student> students = new ArrayList<>() {{    //Czemu podwójne???
+                add(student1);
+                add(student2);
+                add(student3);
+                add(student4);
+        }};
+
+        for (Student student: students) {
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
+            String teacherName = optionalTeacher.orElse(new Teacher("<undefined>")).getName();
+            System.out.println("student: " + student.getName() +" , teacher: " + teacherName);
+        }
+    }
+}
